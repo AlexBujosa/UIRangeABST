69e9b136fe49e91970694b0d3104ee142fe2d35e
Alex Jose Bujosa Cruz <bujosa2014@outlook.com>
2022-06-26 23:28:43 -0400

    Done Features

--- UIRangeABST/Program.cs
+++ UIRangeABST/Program.cs
@@ -5,13 +5,16 @@ class Program
     public static void Main(string[] args)
     {
         bool valido;
-        int salir;
-        Console.WriteLine("Welcome Range App\n");
-
-        Console.Write("Ingrese el rango: ");
-        string range = Console.ReadLine();
+        int salir = 0;
+       
+        
         do
         {
+            Console.Clear();
+            Console.WriteLine("Welcome Range App\n");
+            Console.Write("Ingrese el rango: ");
+            string range = Console.ReadLine();
+            
             Console.WriteLine("/*-----Compare-----");
             Console.WriteLine("1 - Equals");
             Console.WriteLine("2 - Overlaps");
@@ -31,27 +34,59 @@ class Program
                 switch (val)
                 {
                     case 1:
+                        Console.Write("\nIngrese el otro rango: ");
+                        string otroRango = Console.ReadLine();
+                        RangeABST.Range range3 = new RangeABST.Range(range);
+                        Console.WriteLine(range3.Equals(otroRango));
+                        Console.ReadKey();
                         break;
                     case 2:
+                        Console.Write("\nIngrese el otro rango: ");
+                        string otroRango1 = Console.ReadLine();
+                        RangeABST.Range range4 = new RangeABST.Range(range);
+                        Console.WriteLine(range4.OverlapsRange(otroRango1));
+                        Console.ReadKey();
                         break;
                     case 3:
+                        RangeABST.Range range5 = new RangeABST.Range(range);
+                        List<int> values = new List<int>();
+                        Console.Write("Cuantos elementos tendra? ");
+                        int max = int.Parse(Console.ReadLine());
+                        for(int i = 0; i <max; i++)
+                        {
+                            Console.Write("Ingrese el " + (i + 1) + ": ");
+                            int value = int.Parse(Console.ReadLine());
+                            values.Add(value);
+                        }
+                        Console.WriteLine(range5.IntegerRangeContain(values) + range5.NumberContaining(values));
+                        Console.ReadKey();
                         break;
                     case 4:
+                        Console.Write("\nIngrese el otro rango: ");
+                        string otroRango3 = Console.ReadLine();
+                        RangeABST.Range range6 = new RangeABST.Range(range);
+                        Console.WriteLine(range6.ContainsRange(otroRango3));
+                        Console.ReadKey();
                         break;
                     case 5:
+                        RangeABST.Range range1 = new RangeABST.Range(range);
+                        Console.WriteLine(range1.endPoints());
+                        Console.ReadKey();
                         break;
                     case 6:
+                        RangeABST.Range range2 = new RangeABST.Range(range);
+                        Console.WriteLine(range2.GetAllPoints());
+                        Console.ReadKey();
                         break;
                     case 7:
+                        salir = 1;
                         Console.WriteLine("......Saliendo de la aplicación");
-                        
                         break;
                     default:
                         break;
                 }
             }
-            salir = val;
-        } while (valido && salir == 7);
+        } while (salir == 0);
         
        
 
