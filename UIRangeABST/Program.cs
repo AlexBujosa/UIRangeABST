6aaa5036832b9fedf83e826b7ac966b6c3aa872e
Alex Jose Bujosa Cruz <bujosa2014@outlook.com>
2022-06-26 22:58:16 -0400

    Add Menu

--- UIRangeABST/Program.cs
+++ UIRangeABST/Program.cs
@@ -4,12 +4,56 @@ class Program
 {
     public static void Main(string[] args)
     {
+        bool valido;
+        int salir;
         Console.WriteLine("Welcome Range App\n");
 
         Console.Write("Ingrese el rango: ");
         string range = Console.ReadLine();
-
-
+        do
+        {
+            Console.WriteLine("/*-----Compare-----");
+            Console.WriteLine("1 - Equals");
+            Console.WriteLine("2 - Overlaps");
+            Console.WriteLine("3 - IntegerContainRange");
+            Console.WriteLine("4 - ContainRange");
+            Console.WriteLine("\n");
+            Console.WriteLine("/*----- Not Compare-----");
+            Console.WriteLine("5 - EndPoints");
+            Console.WriteLine("6 - GetAllPoints");
+            Console.WriteLine("--------- Salir ----------");
+            Console.WriteLine("7 - Salir de la app");
+            Console.Write("Eliga la opcion que prefiera: ");
+            string strOp = Console.ReadLine();
+            valido = Int32.TryParse(strOp, out int val);
+            if (valido)
+            {
+                switch (val)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        Console.WriteLine("......Saliendo de la aplicación");
+                        
+                        break;
+                    default:
+                        break;
+                }
+            }
+            salir = val;
+        } while (valido && salir == 7);
+        
+       
 
         /*---Compare-----
          * Equlas
@@ -24,8 +68,8 @@ class Program
          * Getallpoints
          * 
          */
-        
-        
+
+
         Console.ReadKey();
 
     }
